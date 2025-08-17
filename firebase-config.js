2665a84d43726654d7abeeb6188173f74f22e53d
Md Farhad Badshah <124345188+mdfarhadbadshah@users.noreply.github.com>
2025-08-17 17:25:27 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
